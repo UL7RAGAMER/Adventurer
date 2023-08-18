a07e18cb489a1a68c1364217774b491bb87ce1c6
UL7RAGAMER <siddkum@outlook.com>
2023-08-18 06:43:23 +0530

    v2

--- Player/fireball.gd
+++ Player/fireball.gd
@@ -1,5 +1,5 @@
 extends Area2D
-var speed: int = 150
+var speed: int = 200
 var direction : Vector2 = Vector2.RIGHT
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
